cccb057185f0d7da533fbc844cae64bf555a5313
wanvidu <wanvidu@gmail.com>
2019-11-20 14:53:21 +0000

    Extra projects added.

---
+++ projects/kotlin_spring/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,15 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+

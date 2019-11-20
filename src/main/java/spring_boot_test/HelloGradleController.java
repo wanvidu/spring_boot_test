5d6265dadad897ffed6f66c5b3977874b714d1fa
wanvidu <wanvidu@gmail.com>
2019-11-20 14:21:14 +0000

    Setup Spring Boot

---
+++ src/main/java/spring_boot_test/HelloGradleController.java
@@ -0,0 +1,14 @@
+package spring_boot_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+
+}
